ba1211ae2937842a17804e4981f5b85d454f9897
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-02-17 15:43:41 +0530

    sample details

---
+++ Reactbasics/src/App.jsx
@@ -0,0 +1,16 @@
+import Details from "./Details"
+
+function App() {
+  
+
+  return (
+    <>
+        <Details/>
+        <p>heiiijbjebr</p>
+        <p>2nd details below</p>
+        <Details/>
+    </>
+  )
+}
+
+export default App
---
+++ Reactbasics/src/Details.jsx
@@ -0,0 +1,9 @@
+function Details(){
+    return (
+        <>
+        <h1>hii </h1>
+        <h2>i am lokesh</h2>
+        </>
+    )
+}
+export default Details
